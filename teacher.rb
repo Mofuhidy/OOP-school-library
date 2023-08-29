8fcdd89faafb8f2990702784823a494f0f71358f
moFuhidy <mo7ammed9290@gmail.com>
2023-08-29 18:26:19 +0300

    Fix teacher arguments

--- teacher.rb
+++ teacher.rb
@@ -1,9 +1,12 @@
 require_relative 'person'
 
 class Teacher < Person
-  def initialize(age, specialization, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission)
+   attr_accessor :type
+   
+  def initialize(age, specialization, name = 'Unknown', parent_permission)
+    super(age, name, parent_permission: parent_permission)
     @specialization = specialization
+    @type = 'Teacher'
   end
 
   def can_use_services?
