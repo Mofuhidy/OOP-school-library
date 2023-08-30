9ad49f42cafbdfe3b33accdad949ec5cb687010a
moFuhidy <mo7ammed9290@gmail.com>
2023-08-30 11:18:29 +0300

    Fix linters errors

--- app.rb
+++ app.rb
@@ -128,17 +128,16 @@ class App
     elsif @people.empty?
       puts 'Sorry, there is no person in the Database. Add a person first in the database'
     else
-
       puts 'Select a book from the following list by number'
       @books.each_with_index { |book, index| puts "#{index} - Title: #{book.title}, Author: #{book.author}" }
       book_index = gets.chomp.to_i
       book = @books[book_index]
-
       puts 'Select a person from the following list by number (not id)'
-      @people.each_with_index { |person, index| puts "#{index} [#{person.type}] - Name: #{person.name}, Age: #{person.age}" }
+      @people.each_with_index do |person, index|
+        puts "#{index} [#{person.type}] - Name: #{person.name}, Age: #{person.age}"
+      end
       person_id = gets.chomp.to_i
       person = @people[person_id]
-
       print 'Date: '
       date = gets.chomp
       rental = Rental.new(date, person, book)
